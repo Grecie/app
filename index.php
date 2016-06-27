72491c9bfd4cba298003eeabfb714dfc0a51b21a
Grecia Cruz <grecicg@gmail.com>
2016-06-27 11:24:01 -0500

    Repositorio inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aplicación</title>
+</head>
+<body>
+	<h1>Página principal</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor veniam in hic tempore iure id tempora impedit neque dolore iste ex distinctio cupiditate, non, laboriosam atque, consectetur repudiandae doloribus ullam.</p>
+	<h2>Encabezado tipo h2</h2>
+</body>
+</html>
